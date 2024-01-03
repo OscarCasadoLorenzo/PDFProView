823bcb9237379fafa3732c7f3ae9850c71e571f9
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2024-01-03 15:00:43 +0100

    chore: :heavy_plus_sign: use-resize-observer & react-window dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from 'react';
+import './App.css';
+import { PdfUrlViewer } from './components/PdfURLViewer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  //TODO: Scale & Page refactoring to JOTAI atoms
+  const [scale, setScale] = useState(1);
+  const [page, setPage] = useState<any>(1);
+  const windowRef: any = useRef();
+  const url = 'sample.pdf';
+
+  const scrollToItem = () => {
+    windowRef.current && windowRef.current.scrollToItem(page - 1, 'start');
+  };
 
   return (
-    <>
+    <div className='App'>
+      <h1>Pdf Viewer</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input value={page} onChange={(e) => setPage(e.target.value)} />
+        <button type='button' onClick={scrollToItem}>
+          goto
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        Zoom
+        <button type='button' onClick={() => setScale((v) => v + 0.1)}>
+          +
+        </button>
+        <button type='button' onClick={() => setScale((v) => v - 0.1)}>
+          -
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <br />
+
+      <PdfUrlViewer url={url} scale={scale} windowRef={windowRef} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import pdfjs from 'pdfjs-dist';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = 'https://d2v5g.csb.app/pdf.worker.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
