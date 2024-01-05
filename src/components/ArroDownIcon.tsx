311e9916ff9fafe652fd4faff02008659d86c821
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2024-01-05 13:40:51 +0100

    refactor: :fire: Icons removed

--- src/components/ArroDownIcon.tsx
+++
@@ -1,4 +0,0 @@
-import { chakra } from '@chakra-ui/react';
-import { BsArrowDown } from 'react-icons/bs';
-
-export const ArrowDownIcon = chakra(BsArrowDown);
--- src/components/ArrowUpIcon.tsx
+++
@@ -1,4 +0,0 @@
-import { chakra } from '@chakra-ui/react';
-import { BsArrowUp } from 'react-icons/bs';
-
-export const ArrowUpIcon = chakra(BsArrowUp);
