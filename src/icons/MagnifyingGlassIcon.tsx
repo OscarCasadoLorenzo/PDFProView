2f8f5c9231594985edb20eee56b6168e9fdbfef5
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2024-01-18 14:10:05 +0100

    style: :lipstick: UI Icons

---
+++ src/icons/MagnifyingGlassIcon.tsx
@@ -0,0 +1,6 @@
+import { chakra } from '@chakra-ui/react';
+import { BsSearch } from 'react-icons/bs';
+
+const MagnifyingGlassIcon = chakra(BsSearch);
+
+export default MagnifyingGlassIcon;
---
+++ src/icons/PlusIcon.tsx
@@ -0,0 +1,5 @@
+import { chakra } from '@chakra-ui/react';
+import { BsPlusLg } from 'react-icons/bs';
+
+const PlusIcon = chakra(BsPlusLg);
+export default PlusIcon;
