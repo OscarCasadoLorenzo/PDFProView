80a2188ff961803bd736cb95f4dfcd27ee72a8f9
OscarCasadoLorenzo <oscarcl.work@gmail.com>
2024-08-28 13:22:38 +0200

    feat(PDFProView): :sparkles: Draw arrow on canvas context function

---
+++ src/features/pdfViewer/utils/canvas-utils.ts
@@ -0,0 +1,32 @@
+const ARROW_HEIGHT = 10;
+
+export function drawArrow(context:any, fromX:number, fromY:number, toX:number, toY:number) {
+
+  context.strokeStyle = 'red'; // Arrow color
+  context.fillStyle = 'red';   // Head arrow color
+  context.lineWidth = 2; 
+
+  const angle = Math.atan2(toY - fromY, toX - fromX);
+
+  // Draw principal arrow line
+  context.beginPath();
+  context.moveTo(fromX, fromY);
+  context.lineTo(toX, toY);
+  context.stroke();
+
+  // Draw arrow head
+  context.beginPath();
+  context.moveTo(toX, toY);
+  context.lineTo(
+      toX - ARROW_HEIGHT * Math.cos(angle - Math.PI / 6),
+      toY - ARROW_HEIGHT* Math.sin(angle - Math.PI / 6)
+  );
+  context.lineTo(
+      toX - ARROW_HEIGHT * Math.cos(angle + Math.PI / 6),
+      toY - ARROW_HEIGHT * Math.sin(angle + Math.PI / 6)
+  );
+  context.lineTo(toX, toY);
+  context.closePath();
+  context.fill();
+}
+
